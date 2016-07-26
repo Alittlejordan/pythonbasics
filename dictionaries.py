97540584a38f7dc94de2459b51b2fd863b41222c
Jordan M <lilj9917@yahoo.com>
2016-07-26 18:42:01 +0000

    python

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+abc = { }
+
+abc
+{}
+tree = "it's a plant"
+
+abc['tree'] = "it's a plant"
+
+{'tree': "it's a plant"}
+abc['tree']
+print (abc)
+
+abc['new key']= " this is the new value"
+
+{'new_key': 'this is a new value', 'tree': "it's a plant", 'list_abc': ['abc'. 123. ['abc',123]]}
---
+++ functionsplay.py
@@ -0,0 +1,18 @@
+# formatting in python
+#x = "this is a string"
+#y = "this is another string"
+#print x + " " +y
+
+#format this word
+#z= "%s is a string", %("whatever")
+
+#we the ____ of the united ___ of_____%("people", "states")
+
+
+def playSong():
+    a= "all %s  me %s are %s faces" %("around", "me", "familar ")
+    print a
+   
+playSong()
+#def playSong():
+    #a= """When in the Course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation""".
